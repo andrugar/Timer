1d3e9bbb3e219f03c9c8142ab44dc44e41d6b0a8
Denis-Etemez <do.etemez@physics.msu.ru>
2018-02-23 14:20:57 +0300

    Create timer for generators

--- timer.py
+++ timer.py
@@ -28,10 +28,30 @@ def timer_v2(t_max):
         return wrapper
     return decorator
 
+def timer_for_generator(func):
+    def wrapper(*args):
+        tic = time.time()
+        result = func(*args)
+        yield from result
+        toc = time.time()
+        print('--- The function: {} -- {} seconds ---'.format(
+              func.__name__, toc-tic)
+    return wrapper
+
 if __name__ == '__main__':
     @timer_v2(2.5)
     def foo(n):
         for i in range(n):
             print(i+1)
-    foo(20)
+        return('hi')
+    
+    print(foo(20))
+    
+    @timer_for_generator
+    def gen(n):
+        for i in range(n):
+            yield i+1
+    
+    for i in gen(30):
+        print(i)
     
